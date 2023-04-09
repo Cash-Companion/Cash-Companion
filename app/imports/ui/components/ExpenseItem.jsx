21f420fdc3bc3498f0f0158767b2a6d48d2ae24c
cathy-kim95 <ckim60@hawaii.edu>
2023-04-08 14:41:30 -1000

    implement deleting capabilities

--- app/imports/ui/components/ExpenseItem.jsx
+++ app/imports/ui/components/ExpenseItem.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card } from 'react-bootstrap';
+import swal from 'sweetalert';
+import { Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { removeItMethod } from '../../api/base/BaseCollection.methods';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
+import { Expenses } from '../../api/expense/ExpenseCollection';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 const ExpenseItem = ({ expense }) => (
@@ -14,7 +17,35 @@ const ExpenseItem = ({ expense }) => (
       <Card.Title>${expense.amount}</Card.Title>
       <Card.Text>{expense.description}</Card.Text>
     </Card.Body>
-    <Link className={COMPONENT_IDS.LIST_EXPENSE_EDIT} to={`/edit-expense/${expense._id}`}> Edit</Link>
+    <Card.Footer>
+      <Button class="btn btn-outline-dark" className={COMPONENT_IDS.LIST_EXPENSE_EDIT} to={`/edit-expense/${expense._id}`}> Edit</Button>
+      <Button class="btn btn-outline-danger"
+        onClick={() => {
+          swal({
+            title: 'Are you sure?',
+            text: 'Once deleted, you will not be able to recover this Spending',
+            icon: 'warning',
+            buttons: true,
+            dangerMode: true,
+          })
+            .then((willDelete) => {
+              if (willDelete) {
+                const collectionName = Expenses.getCollectionName();
+                const instance = expense._id;
+                removeItMethod.callPromise({ collectionName, instance })
+                  .catch(error => swal('Error', error.message, 'error'))
+                  .then(() => {
+                    swal('Success', 'Spending has been deleted!', 'success');
+                  });
+              } else {
+                swal("Don't Worry", 'Your spending data is safe!', 'info');
+              }
+            });
+        }}
+      >
+        Delete
+      </Button>
+    </Card.Footer>
   </Card>
 
 );
